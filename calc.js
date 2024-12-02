be1721dc37a8cdc595684481f832650939a0dc06
AyoubKhachchab <ayoubkhachchab79@gmail.com>
2024-12-02 03:04:44 +0100

    My first simple calculator.

---
+++ calc.js
@@ -0,0 +1,47 @@
+let input = document.getElementById('inputBox');
+
+let buttons = document.querySelectorAll('button');
+
+let string = "";
+
+let arr = Array.from(buttons);
+
+arr.forEach(button => {
+    button.addEventListener('click', (e) =>{
+        if(e.target.innerHTML == '='){
+            string = eval(string);
+            input.value = string;
+        }else if(e.target.innerHTML =='AC'){
+          string = "";
+          input.value = string;
+        }else if(e.target.innerHTML == 'DEL'){
+            string = string.substring(0, string.length-1);
+            input.value = string
+        }
+        else{
+            string += e.target.innerHTML;
+            input.value = string;
+        }
+
+        
+    })
+})
+
+
+
+
+
+
+
+
+
+
+
+/* array.from is for converting element or a lot of elements(called by querySeAll)
+ that are in the html and converting them to an array */
+
+ //element.addEventListener(event, function, options);
+
+ //foreach is to loop into all elements of an array  Syntaxe : forEach(function{})
+
+ //.substring() used to extract caracters from a word  Syntax .substring(start index, end index)
